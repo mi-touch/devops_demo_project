ec69336c4b783c7ee281354763ce423e146aa0f8
Timileyin Adefare <timileyinadefare@yahoo.com>
2024-11-27 13:44:16 +0100

    adding more configs

--- terraform-configs/main.tf
+++ terraform-configs/main.tf
@@ -22,12 +22,3 @@ resource "minikube_cluster" "minikube_docker" {
       "storage-provisioner"
     ]
 }
-
-# resource "minikube_cluster" "minikube_docker" {
-#   driver       = "docker"
-#   cluster_name = "complete-devops-project"
-#   cpus         = 2
-#   memory       = 2048
-
-#   addons = []
-# }
